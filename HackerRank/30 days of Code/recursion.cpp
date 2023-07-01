93cd68744de7b4db99757919160bceccad1cce3f
sudhanshuvishwa <sudhanshu.vishw@gmail.com>
2023-07-01 20:06:12 +0530

    .

---
+++ HackerRank/30 days of Code/recursion.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int factorial(int n) {
+ if(n==0){
+     return 1;
+ }
+ int factn1= factorial(n-1);
+ int fact= factn1*n;
+ return fact;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
--- HackerRank/30 days of Code/review.cpp
+++ HackerRank/30 days of Code/review.cpp
@@ -17,8 +17,7 @@ int main() {
     for(int i=0;i<testcase;i++){
         if(i%2==0){
             char string1[1000];
-            string1=string[i];
-            
+            string1=string[i];  
         }
     }
     return 0;
